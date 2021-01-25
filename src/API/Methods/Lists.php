5390f31054c36d72a6ffc240bde6fe113c0d710c
Vladislav Kolegov <v.kolegov@outlook.com>
2021-01-25 12:23:49 +0700

    [Lists] 'lists.add' method, closes #3

--- src/API/Methods/Lists.php
+++ src/API/Methods/Lists.php
@@ -28,4 +28,33 @@ class Lists
     {
         $this->connection = $connection;
     }
+
+    /**
+     * Creates new address base
+     * @param string $name Future address base name
+     * @param array|null $params
+     * @return int Address Base ID
+     * @throws DashaMailRequestErrorException
+     * @throws \VKolegov\DashaMail\Exceptions\DashaMailConnectionException
+     * @throws \VKolegov\DashaMail\Exceptions\DashaMailInvalidResponseException
+     * @see https://dashamail.ru/api_details/?method=lists.add
+     */
+    public function add($name, $params = [])
+    {
+        $methodName = 'lists.add';
+        if (!is_string($name) || strlen($name) < 1) {
+            throw new \InvalidArgumentException("\$name should be non-empty string");
+        }
+
+        $params = array_merge(
+            [
+                'name' => $name,
+            ],
+            $params
+        );
+
+        $responseData = $this->connection->callMethod($methodName, $params, 'POST');
+
+        return $responseData['list_id'];
+    }
 }
---
+++ tests/API/Methods/ListsTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by vkolegov in PhpStorm
+ * Date: 25/01/2021 04:07
+ */
+
+namespace VKolegov\DashaMail\Tests\API\Methods;
+
+use PHPUnit\Framework\TestCase;
+use VKolegov\DashaMail\API\EntryPoint;
+
+class ListsTest extends TestCase
+{
+    /**
+     * @var \VKolegov\DashaMail\API\Methods\Lists
+     */
+    private $listsApi;
+
+    /**
+     * @var string name of test address list
+     */
+    private $testListName;
+
+    /**
+     * @var int id of test address list
+     */
+    private $testListId;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $apiEntryPoint = new EntryPoint(
+            getenv('DASHAMAIL_USERNAME'),
+            getenv('DASHAMAIL_PASSWORD')
+        );
+
+        $this->listsApi = $apiEntryPoint->Lists();
+
+        $this->testListName = getenv('DASHAMAIL_TEST_LIST');
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        // TODO: delete test list
+    }
+
+    public function testAdd()
+    {
+        $this->testListId = $this->listsApi->add(
+            $this->testListName
+        );
+
+        // Expecting newly created address base ID
+        $this->assertTrue(is_int($this->testListId));
+    }
+
+    public function testAddUnexpectedParams()
+    {
+        $this->testListId = $this->listsApi->add(
+            $this->testListName,
+            [
+                'test' => true,
+                'blabla' => 'albalb'
+            ]
+        );
+
+        // Expecting newly created address list ID
+        $this->assertTrue(is_int($this->testListId));
+    }
+
+    public function testAddRealParams()
+    {
+        $this->testListId = $this->listsApi->add(
+            $this->testListName,
+            [
+                'company' => 'Company Name',
+                'country' => 'Россия',
+                'abuse_name' => 'Spam Complaint Handler',
+                'phone' => '88005553535',
+                'city' => 'Санкт-Петербург'
+            ]
+        );
+
+        // Expecting newly created address list ID
+        $this->assertTrue(is_int($this->testListId));
+    }
+}
